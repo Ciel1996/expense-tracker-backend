ce3ea1f4565b3c31105056d2c581d8dfbd18df47
Dennis Müller <development@cielcat.ch>
2024-12-13 16:51:59 +0100

    working on db init

---
+++ expense-tracker/expense-tracker-db/src/lib.rs
@@ -0,0 +1,174 @@
+pub mod db {
+    /// Currency represents a supported real-world currency.
+    enum Currency {
+        SwissFranc,
+        Euro,
+        UsDollar,
+    }
+
+    pub mod expenses {
+        use diesel::{Insertable, Queryable, Selectable};
+        use serde::Serialize;
+        use crate::db::Currency;
+        use crate::db::users::User;
+
+        /// An expense is an amount of money paid, as well as associated information by a user.
+        /// An expense can either be paid or unpaid. Unpaid expenses should be considered for
+        /// the sum other users have to pay.
+        #[derive(Serialize, Selectable, Queryable, Insertable)]
+        pub struct Expense {
+            owner_id : u64,
+            expense_id : u64,
+            description : String,
+            amount : f64,
+            currency : Currency,
+            is_paid : bool,
+        }
+
+        impl Expense {
+            /// Constructor for Expense
+            pub fn new(user : User, description : String, amount : f64, currency: Currency) -> Expense {
+                Expense {
+                    owner_id: user.user_id(),
+                    expense_id: 1, // TODO: autoincrement
+                    description,
+                    amount,
+                    currency,
+                    is_paid: false
+                }
+            }
+
+            /// Marks this Expense as paid.
+            pub fn pay(&mut self) {
+                self.is_paid = true
+            }
+
+            /// Getter for amount
+            pub fn amount(&self) -> f64 {
+                self.amount
+            }
+
+            /// Getter for is_paid
+            pub fn is_paid(&self) -> bool {
+                self.is_paid
+            }
+
+            /// Getter for description
+            pub fn description(&self) -> &str {
+                &self.description
+            }
+
+            /// Getter for owner_id
+            pub fn owner_id(&self) -> u64 {
+                self.owner_id
+            }
+
+            /// Getter for expense_id
+            pub fn expense_id(&self) -> u64 {
+                self.expense_id
+            }
+        }
+    }
+
+    pub mod pots {
+        use diesel::{Insertable, Queryable, Selectable};
+        use serde::Serialize;
+        use crate::db::Currency;
+        use crate::db::expenses::Expense;
+        use crate::db::users::User;
+
+        /// Represents a pot. A pot is an accumulation of expenses and is owned by a single
+        /// user. A pot can be shared with multiple users. The users can leave a pot
+        /// anytime.
+        /// The owner of a pot need to invite other users to participate in a pot.
+        #[derive(Serialize, Selectable, Queryable, Insertable)]
+        pub struct Pot {
+            owner_id : u64,
+            pot_id: u64,
+            name : String,
+            expenses : Vec<Expense>,
+            default_currency : Currency
+        }
+
+        impl Pot {
+            /// Constructor for Pot
+            pub fn new(user: User, name : String, default_currency : Currency) -> Pot {
+                Pot {
+                    owner_id: user.user_id(),
+                    pot_id: 1, // TODO: autoincrement this
+                    name,
+                    expenses: vec![],
+                    default_currency
+                }
+            }
+
+            /// Adds an expense to the pot
+            pub fn add_expense(&mut self, expense : Expense) {
+                self.expenses.push(expense);
+            }
+
+            /// Removes the Expense with the given expense_id from this pot.
+            pub fn remove_expense(&mut self, expense_id : u64) {
+                if self.expenses.is_empty() {
+                    return;
+                }
+
+                let index = self.expenses
+                    .iter().position(|x| *x.expense_id() == expense_id);
+
+                if let Some(index) = index {
+                    self.expenses.remove(index);
+                }
+            }
+
+            /// Calculates the sum of all unpaid Expenses from the given User's perspective.
+            pub fn get_open_sum(&self, user : User) -> f64 {
+                let mut sum = 0.0;
+
+                for expense in self.expenses {
+                    if expense.owner_id() == user.user_id() {
+                        // if user is owner of the expense
+                        // we can subtract the amount of the expense
+                        // (other users owe the user money)
+                        sum -= expense.amount()
+                    } else {
+                        // if user is not the owner of the expense
+                        // we must add the amount of the expense
+                        // (we owe the other users)
+                        sum += expense.amount();
+                    }
+                }
+
+                sum
+            }
+        }
+    }
+
+    pub mod users {
+        use diesel::{Insertable, Queryable, Selectable};
+        use serde::Serialize;
+
+        /// Defines a user of the systems.
+        /// A user can hold pots, expenses and so on.
+        #[derive(Serialize, Selectable, Queryable, Insertable)]
+        pub struct User {
+            user_id : u64,
+            name : String
+        }
+
+        impl User {
+            /// Constructor for User
+            pub fn new(user_id : u64, name : String) -> User {
+                User {
+                    user_id,
+                    name
+                }
+            }
+
+            /// Getter for user_id
+            pub fn user_id(&self) -> u64 {
+                self.user_id
+            }
+        }
+    }
+}
