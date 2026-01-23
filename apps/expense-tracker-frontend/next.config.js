32a9851c507b0e8694129239135755252ab2eac5
Dennis Müller <development@cielcat.ch>
2026-01-23 13:20:43 +0100

    working on ci/cd and docker

--- apps/expense-tracker-frontend/next.config.js
+++ apps/expense-tracker-frontend/next.config.js
@@ -10,6 +10,8 @@ const nextConfig = {
   // Use this to set Nx-specific options
   // See: https://nx.dev/recipes/next/next-config-setup
   nx: {},
+  // Produce a standalone build suitable for minimal Docker runtime images
+  output: 'standalone',
 };
 
 const plugins = [
